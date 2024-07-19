a4c00646bba73e338bb85548b4341db61f2eb768
王书勤 <22373507@buaa.edu.cn>
2024-07-19 22:30:56 +0800

    v1: add calendar.html & clearDone

--- account/views.py
+++ account/views.py
@@ -5,30 +5,6 @@ from .models import Task
 from .serializers import TaskSerializer
 
 
-def login_view(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
-
-
-def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-    else:
-        form = RegisterForm()
-    return render(request, 'register.html', {'form': form})
-
-
 def home_view(request):
     return render(request, 'home.html', {})
 
